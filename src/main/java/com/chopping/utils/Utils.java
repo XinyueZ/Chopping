69d2ff121ab39a219bb5113c77bd746104dbc47c
Xinyue Zhao <czhao@cellular.de>
2014-09-17 09:17:25 +0200

    Update untils.

--- src/main/java/com/chopping/utils/Utils.java
+++ src/main/java/com/chopping/utils/Utils.java
@@ -1,5 +1,8 @@
 package com.chopping.utils;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 import android.content.Context;
 import android.support.annotation.StringRes;
 import android.widget.Toast;
@@ -57,4 +60,30 @@ public final class Utils {
 	public static void showShortToast(Context context, String message) {
 		Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
 	}
+
+	public static String encode(String _keywords) {
+		try {
+			return URLEncoder.encode(_keywords, "UTF-8");
+		} catch (UnsupportedEncodingException _e1) {
+			return new String(_keywords.trim().replace(" ", "%20").replace("&", "%26").replace(",", "%2c")
+					.replace("(", "%28").replace(")", "%29").replace("!", "%21").replace("=", "%3D")
+					.replace("<", "%3C").replace(">", "%3E").replace("#", "%23").replace("$", "%24")
+					.replace("'", "%27").replace("*", "%2A").replace("-", "%2D").replace(".", "%2E")
+					.replace("/", "%2F").replace(":", "%3A").replace(";", "%3B").replace("?", "%3F")
+					.replace("@", "%40").replace("[", "%5B").replace("\\", "%5C").replace("]", "%5D")
+					.replace("_", "%5F").replace("`", "%60").replace("{", "%7B").replace("|", "%7C")
+					.replace("}", "%7D"));
+		}
+	}
+
+
+	public static String replaceToEncode(String _keywords) {
+		return new String(_keywords.trim().replace(" ", "%20").replace("&", "%26").replace(",", "%2c")
+				.replace("(", "%28").replace(")", "%29").replace("!", "%21").replace("=", "%3D").replace("<", "%3C")
+				.replace(">", "%3E").replace("#", "%23").replace("$", "%24").replace("'", "%27").replace("*", "%2A")
+				.replace("-", "%2D").replace(".", "%2E").replace("/", "%2F").replace(":", "%3A").replace(";", "%3B")
+				.replace("?", "%3F").replace("@", "%40").replace("[", "%5B").replace("\\", "%5C").replace("]", "%5D")
+				.replace("_", "%5F").replace("`", "%60").replace("{", "%7B").replace("|", "%7C").replace("}", "%7D"));
+	}
+
 }
