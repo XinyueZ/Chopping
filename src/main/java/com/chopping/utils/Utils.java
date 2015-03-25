2bdc729a07a579d8a8bf3cf90e116aaef84345c4
XinyueZhao <zhao@eosnewmedia.de>
2015-03-25 12:07:07 +0100

    Add method to generate random number between max, min.

--- src/main/java/com/chopping/utils/Utils.java
+++ src/main/java/com/chopping/utils/Utils.java
@@ -7,6 +7,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.Random;
 
 import android.content.Context;
 import android.content.Intent;
@@ -249,5 +250,28 @@ public final class Utils {
 	 */
 	public static float convertPixelsToDp(Context context, float px) {
 		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, px, context.getResources().getDisplayMetrics());
+	}
+
+
+	/**
+	 * Returns a pseudo-random number between min and max, inclusive.
+	 * The difference between min and max can be at most
+	 * <code>Integer.MAX_VALUE - 1</code>.
+	 *
+	 * @param min Min value
+	 * @param max Max value.  Must be greater than min.
+	 * @return Integer between min and max, inclusive.
+	 * @see java.util.Random#nextInt(int)
+	 */
+	public static int randInt(int min, int max) {
+
+		// Usually this can be a field rather than a method variable
+		Random rand = new Random();
+
+		// nextInt is normally exclusive of the top value,
+		// so add 1 to make it inclusive
+		int randomNum = rand.nextInt((max - min) + 1) + min;
+
+		return randomNum;
 	}
 }
